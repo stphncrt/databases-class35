88e5813772cf54e8132eed18eddea4fe0a81693d
stphn <cretestephen@gmail.com>
2022-03-29 21:30:19 +0200

    ex3_joins completed

---
+++ Week2/ex3_joins.js
@@ -0,0 +1,37 @@
+import { createConnection } from 'mysql';
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'hyfuser',
+    password: 'hyfpassword',
+    database: 'bookdb',
+});
+
+connection.connect();
+
+const execQuery = (query) => {
+    connection.query(query, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+};
+
+const PRINT_ALL_AUTHORS_AND_MENTORS = `
+  SELECT author_name, mentor FROM authors;
+`;
+
+const PRINT_ALL_AUTHORS_COLUMNS_AND_PAPER_TITLE = `
+  SELECT authors.*, research_Papers.paper_title 
+  FROM authors
+  LEFT JOIN auth_res_papers 
+  ON auth_res_papers.author_no = authors.author_no
+  LEFT JOIN research_Papers
+  ON auth_res_papers.paper_id = research_Papers.paper_id;
+`;
+
+connection.connect();
+
+execQuery(PRINT_ALL_AUTHORS_AND_MENTORS);
+execQuery(PRINT_ALL_AUTHORS_COLUMNS_AND_PAPER_TITLE);
+
+connection.end();
