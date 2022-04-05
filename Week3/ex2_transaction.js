a2b9fd95699921e05469a5d825f2c68208224cb6
stphn <cretestephen@gmail.com>
2022-04-05 13:45:47 +0200

    Connection added to ex2

--- Week3/ex2_transaction.js
+++ Week3/ex2_transaction.js
@@ -1,35 +1,38 @@
 import { createConnection } from "mysql";
+import Connection from "mysql/lib/Connection";
 import util from "util";
 
 const connection = createConnection({
-    host: "localhost",
-    user: "hyfuser",
-    password: "hyfpassword",
-    database: "accountdb",
+	host: "localhost",
+	user: "hyfuser",
+	password: "hyfpassword",
+	database: "accountdb",
 });
 const execQuery = util.promisify(connection.query.bind(connection));
-const transferMoney = async() => {
-    const AUTO_COMMIT_OFF = `SET AUTOCOMMIT = 0;`;
-    const START = `START TRANSACTION;`;
-    const ROLLBACK = `ROLLBACK;`;
-    const COMMIT = `COMMIT;`;
-    const AUTO_COMMIT_ON = `SET AUTOCOMMIT = 1;`;
-    const CANCEL_FK_CHECK = `SET FOREIGN_KEY_CHECKS = 0;`;
-    const UPDATE_ACCOUNT = `UPDATE account SET balance  = 4900 WHERE account_number = 101;`;
-    const INSERT_TRANSFER = `INSERT INTO account_changes VALUES(2, 101, 100, '2022-04-05', 'money transfer successfully');`;
-
-    try {
-        await execQuery(AUTO_COMMIT_OFF);
-        await execQuery(START);
-        await execQuery(CANCEL_FK_CHECK);
-        await execQuery(INSERT_TRANSFER);
-        await execQuery(UPDATE_ACCOUNT);
-        await execQuery(COMMIT);
-        await execQuery(AUTO_COMMIT_ON);
-    } catch (err) {
-        console.log(err);
-        execQuery(ROLLBACK);
-    }
+const transferMoney = async () => {
+	const AUTO_COMMIT_OFF = `SET AUTOCOMMIT = 0;`;
+	const START = `START TRANSACTION;`;
+	const ROLLBACK = `ROLLBACK;`;
+	const COMMIT = `COMMIT;`;
+	const AUTO_COMMIT_ON = `SET AUTOCOMMIT = 1;`;
+	const CANCEL_FK_CHECK = `SET FOREIGN_KEY_CHECKS = 0;`;
+	const UPDATE_ACCOUNT = `UPDATE account SET balance  = 4900 WHERE account_number = 101;`;
+	const INSERT_TRANSFER = `INSERT INTO account_changes VALUES(2, 101, 100, '2022-04-05', 'money transfer successfully');`;
+	connection.connect();
+	try {
+		await execQuery(AUTO_COMMIT_OFF);
+		await execQuery(START);
+		await execQuery(CANCEL_FK_CHECK);
+		await execQuery(INSERT_TRANSFER);
+		await execQuery(UPDATE_ACCOUNT);
+		await execQuery(COMMIT);
+		await execQuery(AUTO_COMMIT_ON);
+	} catch (err) {
+		console.log(err);
+		execQuery(ROLLBACK);
+		connection.end();
+	}
+	connection.end();
 };
 
 transferMoney();
--- Week3/ex2_transactions-insert-values.js
+++ Week3/ex2_transactions-insert-values.js
@@ -2,26 +2,26 @@ import { createConnection } from "mysql";
 import util from "util";
 
 const connection = createConnection({
-    host: "localhost",
-    user: "hyfuser",
-    password: "hyfpassword",
-    database: "accountdb",
+	host: "localhost",
+	user: "hyfuser",
+	password: "hyfpassword",
+	database: "accountdb",
 });
 const execQuery = util.promisify(connection.query.bind(connection));
-const seedDatabase = async() => {
-    const CANCEL_FK_CHECK = "SET FOREIGN_KEY_CHECKS = 0;";
-    const INSERT_ACCOUNT_TABLE = `INSERT INTO  account VALUES (101, 5000),(102, 3000),(103, 3000),(104, 6000),(105, 8000);`;
-    const INSERT_ACCOUNT_CHANGES_TABLE = `INSERT INTO account_changes VALUES (1, 102, 1000, '2022-04-04', 'money transfer successfully')`;
-    try {
-        connection.connect();
-        await execQuery(CANCEL_FK_CHECK);
-        await execQuery(INSERT_ACCOUNT_TABLE);
-        await execQuery(INSERT_ACCOUNT_CHANGES_TABLE);
-    } catch (error) {
-        console.log(error);
-        connection.end();
-    }
-    connection.end();
+const seedDatabase = async () => {
+	const CANCEL_FK_CHECK = "SET FOREIGN_KEY_CHECKS = 0;";
+	const INSERT_ACCOUNT_TABLE = `INSERT INTO  account VALUES (101, 5000),(102, 3000),(103, 3000),(104, 6000),(105, 8000);`;
+	const INSERT_ACCOUNT_CHANGES_TABLE = `INSERT INTO account_changes VALUES (1, 102, 1000, '2022-04-04', 'money transfer successfully')`;
+	connection.connect();
+	try {
+		await execQuery(CANCEL_FK_CHECK);
+		await execQuery(INSERT_ACCOUNT_TABLE);
+		await execQuery(INSERT_ACCOUNT_CHANGES_TABLE);
+	} catch (error) {
+		console.log(error);
+		connection.end();
+	}
+	connection.end();
 };
 
 seedDatabase();
